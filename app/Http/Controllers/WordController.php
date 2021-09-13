eb1c2ec1866a13c0e5d8f5bd87e34c7fe73a8175
YukiYouda <youda0518@yahoo.co.jp>
2021-09-13 13:28:54 +0900

    greeting 機能実装

---
+++ app/Http/Controllers/WordController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class WordController extends Controller
+{
+    public function word ($word) {
+        return view('word', ['word' => $word]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('comments/{time}', [App\Http\Controllers\GreetController::class, 'greet']);
+
+Route::get('comments/freeword/{word}', [App\Http\Controllers\WordController::class, 'word']
+);
+
+Route::get('comments/random', [App\Http\Controllers\GreetController::class, 'random']);
